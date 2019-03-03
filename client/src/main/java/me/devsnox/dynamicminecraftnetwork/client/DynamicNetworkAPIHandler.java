cea8be1a06efaa0754ea12ab37ef78ef40d496de
LartyHD <lartyhd@gmail.com>
2019-03-04 00:33:33 +0100

    Fix save

--- client/src/main/java/me/devsnox/dynamicminecraftnetwork/client/DynamicNetworkAPIHandler.java
+++ client/src/main/java/me/devsnox/dynamicminecraftnetwork/client/DynamicNetworkAPIHandler.java
@@ -5,40 +5,42 @@ import de.d3adspace.skylla.client.SkyllaClient;
 import me.devsnox.dynamicminecraftnetwork.api.DynamicNetworkAPI;
 import me.devsnox.dynamicminecraftnetwork.client.handlers.ClientSchematicHandler;
 import me.devsnox.dynamicminecraftnetwork.client.io.DataManager;
-import me.devsnox.dynamicminecraftnetwork.commons.packets.RequestSchematicPacket;
 
 import java.util.UUID;
 import java.util.function.Consumer;
 
-public class DynamicNetworkAPIHandler implements DynamicNetworkAPI {
-
-    private DataManager dataManager;
-    private ClientSchematicHandler schematicHandler;
-    private SkyllaClient skyllaClient;
-
-    public DynamicNetworkAPIHandler(ClientSchematicHandler schematicHandler, SkyllaClient skyllaClient) {
-        this.dataManager = new DataManager("DynamicStorage");
-        this.schematicHandler = schematicHandler;
-        this.skyllaClient = skyllaClient;
-    }
-
-
-
-    @Override
-    public void hasSchematic(UUID uuid, Consumer<Boolean> result) {
-        this.schematicHandler.addToBooleanReceiveQuery(uuid, result);
-        this.skyllaClient.sendPacket(new RequestSchematicPacket(uuid));
-    }
-
-    @Override
-    public void getSchematic(UUID uuid, Consumer<Schematic> request) {
-        request.accept(this.dataManager.load(uuid));
-    }
-
-    @Override
-    public void saveSchematic(UUID uuid, Schematic schematic) {
-        //this.dataManager.save(uuid, schematic);
-    }
+public class DynamicNetworkAPIHandler implements DynamicNetworkAPI
+{
+	private final DataManager dataManager;
+	private final ClientSchematicHandler schematicHandler;
+	private final SkyllaClient skyllaClient;
+	
+	public DynamicNetworkAPIHandler(ClientSchematicHandler schematicHandler, SkyllaClient skyllaClient)
+	{
+		this.dataManager = new DataManager("DynamicStorage");
+		this.schematicHandler = schematicHandler;
+		this.skyllaClient = skyllaClient;
+	}
+	
+	@Override
+	public void hasSchematic(UUID uuid, Consumer<Boolean> result)
+	{
+		result.accept(this.dataManager.exists(uuid));
+//		this.schematicHandler.addToBooleanReceiveQuery(uuid, result);
+//		this.skyllaClient.sendPacket(new RequestSchematicPacket(uuid));
+	}
+	
+	@Override
+	public void getSchematic(UUID uuid, Consumer<Schematic> request)
+	{
+		request.accept(this.dataManager.load(uuid));
+	}
+	
+	@Override
+	public void saveSchematic(UUID uuid, Schematic schematic)
+	{
+		this.dataManager.save(uuid, schematic);
+	}
 
     /*@Override
     public void getSchematic(UUID uuid, Consumer<Schematic> request) {
@@ -51,6 +53,4 @@ public class DynamicNetworkAPIHandler implements DynamicNetworkAPI {
         this.skyllaClient.sendPacket(new SchematicPacket(uuid, schematic));
 
     }*/
-
-
 }
--- server/src/main/java/me/devsnox/dynamicminecraftnetwork/server/data/DataManager.java
+++ server/src/main/java/me/devsnox/dynamicminecraftnetwork/server/data/DataManager.java
@@ -4,57 +4,67 @@ import com.boydti.fawe.object.schematic.Schematic;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.UUID;
 
-public class DataManager {
-
-    private File directory;
-
-    public DataManager(String name) {
-        this.directory = new File(name);
-
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
-    }
-
-    public boolean exists(UUID uuid) {
-        return new File(this.directory, uuid + ".schematic").exists();
-    }
-
-    public void save(UUID uuid, Schematic schematic) {
-        try {
-            schematic.save(new File(directory, uuid.toString()), ClipboardFormat.SCHEMATIC);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public Schematic load(UUID uuid) {
-        if (exists(uuid)) {
-            return load(new File(this.directory, uuid.toString()));
-        }
-
-        return getDefaultSchematic();
-    }
-
-    private Schematic getDefaultSchematic() {
-        File file = new File(this.directory.getParentFile(), "default.schematic");
-
-        if (!file.exists() || file.isDirectory()) {
-            throw new RuntimeException("IO NetworkSchematic Exception - No default file found!"); //TODO Better message
-        }
-
-        return load(file);
-    }
-
-    private Schematic load(File file) {
-        try {
-            return ClipboardFormat.SCHEMATIC.load(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+public class DataManager
+{
+	private final File directory;
+	
+	public DataManager(String name)
+	{
+		this.directory = new File(name);
+		if (!this.directory.exists())
+		{
+			this.directory.mkdirs();
+		}
+	}
+	
+	public boolean exists(UUID uuid)
+	{
+		return new File(this.directory, uuid + ".schematic").exists();
+	}
+	
+	public void save(UUID uuid, Schematic schematic)
+	{
+		try
+		{
+			schematic.save(new File(this.directory, uuid.toString()), ClipboardFormat.SCHEMATIC);
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public Schematic load(UUID uuid)
+	{
+		if (exists(uuid))
+		{
+			return load(new File(this.directory, uuid.toString()));
+		}
+		return getDefaultSchematic();
+	}
+	
+	private Schematic getDefaultSchematic()
+	{
+		File file = new File(this.directory, "default.schematic");
+		if (!file.exists() || file.isDirectory())
+		{
+			throw new RuntimeException("IO NetworkSchematic Exception - No default file found!"); //TODO Better message
+		}
+		return load(file);
+	}
+	
+	private Schematic load(File file)
+	{
+		try
+		{
+			return new Schematic(ClipboardFormat.SCHEMATIC.getReader(new FileInputStream(file)).read(null));
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
